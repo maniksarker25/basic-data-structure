199fe5c8ba98112350a1d390e402d20bf4cf5a0e
manik <maniksarker652@gmail.com>
2024-12-08 00:24:18 +0600

    learn about quadratic complexity

---
+++ time-complexity/quadratic_complexity.cpp
@@ -0,0 +1,19 @@
+// quadratic complexity / order of n square
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "Hello world"; // complexity O(n *n);
+        }
+    }
+    return 0;
+}

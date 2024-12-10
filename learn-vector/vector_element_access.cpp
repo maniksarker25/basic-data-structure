cbbb3ac666d415c4c2755a3a176e52570adb94dd
manik <maniksarker652@gmail.com>
2024-12-10 19:55:53 +0600

    lear vector element access

---
+++ learn-vector/vector_element_access.cpp
@@ -0,0 +1,15 @@
+// vector element access
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    vector<int> v = {5, 35, 63, 33, 56};
+    cout << v[3]; // access vector element
+    cout << endl;
+    cout << v.back() << " " << v.front(); // access first and last element
+    return 0;
+}

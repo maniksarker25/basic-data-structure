d4843504e1b14d01658ed9a1e88df9c0c70cbcc9
manik <maniksarker652@gmail.com>
2024-12-11 22:49:54 +0600

    solve range sum query

--- prefix-sum-and-binary-search/Y_Range_sum_query.cpp
+++ prefix-sum-and-binary-search/Y_Range_sum_query.cpp
@@ -42,7 +42,7 @@ int main()
 {
     int n, q;
     cin >> n >> q;
-    vector<int> v(n + 1);
+    vector<long long int> v(n + 1);
     for (int i = 1; i <= n; i++)
     {
         cin >> v[i];
@@ -50,7 +50,7 @@ int main()
 
     // find prefix sum values
 
-    vector<int> pre(n + 1);
+    vector<long long int> pre(n + 1);
     // manually assign the first value of prefix sum
     pre[1] = v[1];
     for (int i = 2; i <= n; i++)
@@ -58,24 +58,21 @@ int main()
         pre[i] = pre[i - 1] + v[i];
     }
 
-    for (int i = 1; i <= n; i++)
-    {
-        cout << pre[i] << ' ';
-    }
-
     while (q--)
     {
         int l, r;
         cin >> l >> r;
-        int sum = 0;
+        long long int sum;
+        if (l == 1)
+        {
+            sum = pre[r];
+        }
+        else
+        {
+            sum = pre[r] - pre[l - 1];
+        }
 
-        // remove this loop , make it without loop => using prefix sum => ======================
-        // for (int i = l; i <= r; i++)
-        // {
-        //     sum += v[i];
-        // }
-
-        // cout << sum << endl;
+        cout << sum << endl;
     }
 
     return 0;

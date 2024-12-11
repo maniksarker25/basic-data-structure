16c6b7ce99338774f9c88564e969a87184a737d3
manik <maniksarker652@gmail.com>
2024-12-11 21:38:08 +0600

    learn about prefix sum and find prefix sum

--- prefix-sum-and-binary-search/Y_Range_sum_query.cpp
+++ prefix-sum-and-binary-search/Y_Range_sum_query.cpp
@@ -33,6 +33,7 @@
 //     return 0;
 // }
 
+// optimize way with maintain time complexity
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -47,17 +48,34 @@ int main()
         cin >> v[i];
     }
 
+    // find prefix sum values
+
+    vector<int> pre(n + 1);
+    // manually assign the first value of prefix sum
+    pre[1] = v[1];
+    for (int i = 2; i <= n; i++)
+    {
+        pre[i] = pre[i - 1] + v[i];
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << pre[i] << ' ';
+    }
+
     while (q--)
     {
         int l, r;
         cin >> l >> r;
         int sum = 0;
 
-        for (int i = l; i <= r; i++)
-        {
-            sum += v[i];
-        }
-        cout << sum << endl;
+        // remove this loop , make it without loop => using prefix sum => ======================
+        // for (int i = l; i <= r; i++)
+        // {
+        //     sum += v[i];
+        // }
+
+        // cout << sum << endl;
     }
 
     return 0;

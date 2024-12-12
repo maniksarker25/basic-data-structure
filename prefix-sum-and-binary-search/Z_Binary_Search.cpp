f1c31fb4d4d4a63f120034c08aaa49e26a565af0
manik <maniksarker652@gmail.com>
2024-12-12 22:08:19 +0600

    learn about binary search and use binary search in code

--- prefix-sum-and-binary-search/Z_Binary_Search.cpp
+++ prefix-sum-and-binary-search/Z_Binary_Search.cpp
@@ -1,39 +1,41 @@
-#include <bits/stdc++.h>
+// binary search in bruteforce way----------------------------
 
-using namespace std;
+// #include <bits/stdc++.h>
 
-int main()
-{
+// using namespace std;
 
-    int n, q;
-    cin >> n >> q;
-    int a[n];
+// int main()
+// {
 
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
+//     int n, q;
+//     cin >> n >> q;
+//     int a[n];
 
-    for (int i = 0; i < q; i++)
-    {
-        int x;
-        cin >> x;
-        int flag = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (a[i] == x)
-            {
-                flag = 1;
-            }
-        }
-        if (flag == 1)
-        {
-            cout << "found" << endl;
-        }
-        else
-        {
-            cout << "not found" << endl;
-        }
-    }
-    return 0;
-}
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> a[i];
+//     }
+
+//     for (int i = 0; i < q; i++)
+//     {
+//         int x;
+//         cin >> x;
+//         int flag = 0;
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (a[i] == x)
+//             {
+//                 flag = 1;
+//             }
+//         }
+//         if (flag == 1)
+//         {
+//             cout << "found" << endl;
+//         }
+//         else
+//         {
+//             cout << "not found" << endl;
+//         }
+//     }
+//     return 0;
+// }

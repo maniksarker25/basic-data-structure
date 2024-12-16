8797728100401bdf61c2890ac9f01d5b5e8385aa
manik <maniksarker652@gmail.com>
2024-12-16 21:21:18 +0600

    successfully insert node in any position in linked list

---
+++ linked-list/insert_at_any_pos.cpp
@@ -0,0 +1,64 @@
+// insert at head in linked list---------
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+// function for insert at head
+void insert_at_any_pos(Node *&head, int idx, int val)
+{
+
+    Node *newNode = new Node(val);
+
+    // take head to the inx -1 position
+    Node *temp = head;
+    for (int i = 0; i < idx - 1; i++)
+    {
+        temp = temp->next;
+    }
+    // set new node next = the address of idx node ---
+    newNode->next = temp->next;
+    // set temp ar next = new node
+    temp->next = newNode;
+};
+
+// print linked list
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    insert_at_any_pos(head, 2, 100);
+    insert_at_any_pos(head, 2, 200);
+
+    print_linked_list(head);
+    return 0;
+}

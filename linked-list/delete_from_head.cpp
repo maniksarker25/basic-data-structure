d1d0940116420aee2bc3ae27aeb04ee30022206d
manik <maniksarker652@gmail.com>
2024-12-17 23:41:11 +0600

    perfectly delete node from head

---
+++ linked-list/delete_from_head.cpp
@@ -0,0 +1,77 @@
+// delete from head-----------
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+// function for insert at tail
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    // create new node
+    Node *newNode = new Node(val);
+
+    // handle corner case
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // make connection with current last node with new node
+    tail->next = newNode;
+
+    // then move the tail to new node
+    tail = newNode;
+};
+
+// print linked list
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+// delete from head
+void delete_from_head(Node *&head)
+{
+    Node *delete_node = head;
+    head = head->next;
+    delete delete_node;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    delete_from_head(head);
+    print_linked_list(head);
+
+    return 0;
+}

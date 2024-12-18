290028b6c9fdcdbff5e81beffc23220d72f6763f
manik <maniksarker652@gmail.com>
2024-12-18 19:36:21 +0600

    successfully sort an array using selection sort

---
+++ linked-list/selection_sort.cpp
@@ -0,0 +1,33 @@
+// use selection sort with array
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    // selection sort
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] > a[j])
+            {
+                swap(a[i], a[j]);
+            }
+        }
+    }
+    // print array
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}

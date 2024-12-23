38042fbaff91b95a1582e94865a147097afd5c29
manik <maniksarker652@gmail.com>
2024-12-23 19:53:40 +0600

    learn more about modifier and implement those

--- linked-list-stl/list_modifier.cpp
+++ linked-list-stl/list_modifier.cpp
@@ -11,15 +11,25 @@ int main()
     l2 = l;
     // l2.assign(l.begin(), l.end());
 
-    // push back => insert at tail
+    // push back => insert at tail------------
     l2.push_back(40);
-    // push font => insert at head
+    // push font => insert at head---------
     l2.push_front(5);
 
-    // pop back => delete from tail
+    // pop back => delete from tail-------
     // l2.pop_back();
-    // pop front => delete from head
+    // pop front => delete from head------
     l2.pop_front();
+
+    // insert at any -------------------
+    l2.insert(next(l2.begin(), 2), 25);
+
+    // we can insert a list also -----
+    list<int> l3 = {200, 300};
+    l2.insert(next(l2.begin(), 2), l3.begin(), l3.end());
+    // also we can insert a vector ---------------
+    // access element/node -----------------
+    // cout << *next(l.begin(), 2);
 
     for (int val : l2)
     {

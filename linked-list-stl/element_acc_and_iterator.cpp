6ce860904f0d1853066f68aae3b64a058d1ba010
manik <maniksarker652@gmail.com>
2024-12-23 20:18:51 +0600

    learn about element access for stl list

---
+++ linked-list-stl/element_acc_and_iterator.cpp
@@ -0,0 +1,23 @@
+// element access and iterator
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    list<int> l = {10, 20, 30, 40, 50, 60, 10, 28};
+
+    // print list last element---------------
+    // cout << l.back();
+    // print list first element--------------
+    // cout << l.front();
+
+    // print any pos------------
+    // cout << *next(l.begin(), 2);
+
+    // print first element using iterator
+    // cout << *l.begin();
+
+    return 0;
+}

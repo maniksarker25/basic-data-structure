1959a0492cff6f8828596bf288285c69e7aa5bbb
manik <maniksarker652@gmail.com>
2024-12-24 20:23:21 +0600

    sort doubly linked list

---
+++ practice-doubly-list/problem-5.cpp
@@ -0,0 +1,91 @@
+/*
+Question: Take a doubly linked list as input and sort it in ascending order. Then print the list.
+
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+// insert at tail
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+// print
+void print_doubly(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+// reverse doubly
+
+void sort_doubly(Node *head, Node *tail)
+{
+    for (Node *i = head; i->next != NULL; i = i->next)
+    {
+        for (Node *j = i->next; j != NULL; j = j->next)
+        {
+            if (i->val > j->val)
+            {
+                swap(i->val, j->val);
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    int count = 0;
+
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+        count++;
+    }
+
+    sort_doubly(head, tail);
+    print_doubly(head);
+
+    return 0;
+}

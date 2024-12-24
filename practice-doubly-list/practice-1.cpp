60d8327decc9e571959b9cdf92846de617f0f5df
manik <maniksarker652@gmail.com>
2024-12-24 19:59:30 +0600

    successfully solved first problem

--- practice-doubly-list/practice-1.cpp
+++ practice-doubly-list/practice-1.cpp
@@ -36,6 +36,33 @@ void insert_at_tail(Node *&head, Node *&tail, int val)
     tail = newNode;
 }
 
+// check same
+void check_same(Node *head1, Node *head2)
+{
+
+    int is_same = 1;
+    while (head1 != NULL)
+    {
+
+        if (head1->val != head2->val)
+        {
+            is_same = 0;
+            break;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+
+    if (is_same == 1)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
+
 int main()
 {
 
@@ -45,14 +72,38 @@ int main()
     Node *tail2 = NULL;
 
     int val;
+    int first_list_cout = 0;
+    int second_list_count = 0;
 
     while (true)
     {
         cin >> val;
         if (val == -1)
         {
-            return;
+            break;
         }
+        insert_at_tail(head1, tail1, val);
+        first_list_cout++;
+    }
+
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head2, tail2, val);
+        second_list_count++;
+    }
+
+    if (first_list_cout != second_list_count)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        check_same(head1, head2);
     }
 
     return 0;

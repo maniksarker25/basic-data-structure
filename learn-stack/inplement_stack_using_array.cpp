a22c29bcb06bdf55ef18c4cf2e9d047104f08227
manik <maniksarker652@gmail.com>
2025-01-05 16:01:18 +0600

    create a class with vector for crating stack and also make fucntions

--- learn-stack/inplement_stack_using_array.cpp
+++ learn-stack/inplement_stack_using_array.cpp
@@ -2,6 +2,38 @@
 
 using namespace std;
 
+class myStack
+{
+public:
+    vector<int> v;
+
+    // functions for push
+    void push(int val)
+    {
+        v.push_back(val);
+    }
+    // function for pop
+    void pop()
+    {
+        v.pop_back();
+    }
+    // function for return top value
+    int top()
+    {
+        return v.back();
+    }
+    // function for return size
+    int size()
+    {
+        return v.size();
+    }
+    // function for check empty
+    bool empty()
+    {
+        return v.empty();
+    }
+};
+
 int main()
 {
     cout << "nice";

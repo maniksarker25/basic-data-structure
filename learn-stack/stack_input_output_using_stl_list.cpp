e5611b73c4f31a224ded62371006078878e8463e
manik <maniksarker652@gmail.com>
2025-01-05 17:51:13 +0600

    take input output with stl list in stack

---
+++ learn-stack/stack_input_output_using_stl_list.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class myStack
+{
+public:
+    list<int> l;
+
+    // functions for push
+    void push(int val)
+    {
+        l.push_back(val);
+    }
+    // function for pop
+    void pop()
+    {
+        l.pop_back();
+    }
+    // function for return top value
+    int top()
+    {
+        return l.back();
+    }
+    // function for return size
+    int size()
+    {
+        return l.size();
+    }
+    // function for check empty
+    bool empty()
+    {
+        return l.empty();
+    }
+};
+
+int main()
+{
+    myStack st;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+    // output stack
+    while (!st.empty())
+    {
+        if (!st.empty())
+        {
+            cout << st.top() << " ";
+            st.pop();
+        }
+    }
+
+    return 0;
+}

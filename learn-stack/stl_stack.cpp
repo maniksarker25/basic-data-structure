772699e60ee377f9afc9cb5175d3d85e8849c631
manik <maniksarker652@gmail.com>
2025-01-05 22:20:00 +0600

    work with stack stl

---
+++ learn-stack/stl_stack.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // declare stack
+    stack<int> st;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        st.push(val);
+    }
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+
+    return 0;
+}

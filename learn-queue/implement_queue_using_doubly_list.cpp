619cc0510ab8b71527038e1af9fc2b8d791b0290
manik <maniksarker652@gmail.com>
2025-01-06 00:09:05 +0600

    implement queue using doubly linked list

---
+++ learn-queue/implement_queue_using_doubly_list.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+class myQueue
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+
+public:
+    // function for push
+    void push(int val)
+    {
+        sz++;
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+    // function for pop
+    void pop()
+    {
+        sz--;
+        Node *deletedNode = head;
+        head = head->next;
+        delete deletedNode;
+        if (head == NULL)
+        {
+            tail = NULL;
+            return;
+        }
+        head->prev = NULL;
+    }
+
+    // function for front
+    int front()
+    {
+        return head->val;
+    }
+
+    // function for back
+    int back()
+    {
+        return tail->val;
+    }
+
+    // function for size
+    int size()
+    {
+        return sz;
+    }
+
+    // check empty
+    bool empty()
+    {
+        return head == NULL;
+    }
+};
+
+int main()
+{
+    myQueue q;
+    int n;
+    cin >> n;
+    // take input a queue----------
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        q.push(val);
+    }
+
+    // print queue ----------
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    return 0;
+}

cbcafb87789a40b2d7cc11138cb77afab54c1d67
manik <maniksarker652@gmail.com>
2025-01-05 23:45:35 +0600

    initialize a stack claass with singly linked list and make function for push

---
+++ learn-queue/implement_queue_using_singly_list.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class myQueue
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    // function for push
+    void push(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+    }
+    // function for pop
+    
+};
+
+int main()
+{
+    cout << "nice";
+    return 0;
+}

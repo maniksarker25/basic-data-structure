13553151bae8599644318723c07fb2d672c50b03
manik <maniksarker652@gmail.com>
2025-01-12 23:50:28 +0600

    count node using recursion

---
+++ binary-tree/count_node_using_recursion.cpp
@@ -0,0 +1,103 @@
+// count node using recursion
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *intput_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+    {
+        root = NULL;
+    }
+    else
+    {
+        root = new Node(val);
+    }
+
+    queue<Node *> q;
+    if (root)
+    {
+        q.push(root);
+    }
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft, *myRight;
+        if (l == -1)
+        {
+            myLeft = NULL;
+        }
+        else
+        {
+            myLeft = new Node(l);
+        }
+        if (r == -1)
+        {
+            myRight = NULL;
+        }
+        else
+        {
+            myRight = new Node(r);
+        }
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        if (p->left)
+        {
+            q.push(p->left);
+        }
+        if (p->right)
+        {
+            q.push(p->right);
+        }
+    }
+
+    return root;
+}
+
+// cout using recursion
+
+int count_node(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int l = count_node(root->left);
+    int r = count_node(root->right);
+    return l + r + 1;
+}
+
+int main()
+{
+
+    Node *root = intput_tree();
+
+    int total_node = count_node(root);
+    cout << total_node;
+
+    return 0;
+}

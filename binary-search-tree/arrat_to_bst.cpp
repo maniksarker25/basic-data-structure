a3964c8cd0edf8f406007b3df0074d377d9dbe77
manik <maniksarker652@gmail.com>
2025-01-19 00:06:10 +0600

    convert a sorted array to bst

---
+++ binary-search-tree/arrat_to_bst.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void level_order_print(Node *root)
+{
+    queue<Node *> q;
+    if (root)
+    {
+        q.push(root);
+    }
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+
+        cout << f->val << " ";
+        if (f->left)
+        {
+            q.push(f->left);
+        }
+        if (f->right)
+        {
+            q.push(f->right);
+        }
+    }
+}
+
+Node *convert_array_to_bst(int a[], int n, int l, int r)
+{
+    if(l > r){
+        return NULL;
+    }
+    int mid = (l + r) / 2;
+    Node *root = new Node(a[mid]);
+    Node *left_root = convert_array_to_bst(a, n, l, mid - 1);
+    Node *right_root = convert_array_to_bst(a, n, mid + 1, r);
+    root->left = left_root;
+    root->right = right_root;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    Node *root = convert_array_to_bst(a, n, 0, n - 1);
+    level_order_print(root);
+
+    return 0;
+}

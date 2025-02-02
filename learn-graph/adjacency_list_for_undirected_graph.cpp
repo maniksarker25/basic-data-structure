a45ec04ee8f7c45bdee6daec6d93e20b631782d8
manik <maniksarker652@gmail.com>
2025-02-03 00:05:25 +0600

    input undirected graph using adjacency list

---
+++ learn-graph/adjacency_list_for_undirected_graph.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<int> adj_list[n];
+    while (e--)
+    {
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+
+    }
+
+    // print after input
+    for(int i = 0;i<n;i++){
+        cout << i << " =>";
+        for(int val : adj_list[i]){
+            cout <<val << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
